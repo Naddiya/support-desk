c758e49ae874b75e18bf10cb5b8eaba2a370eda6
Naddiya <nadiamedkouriz@gmail.com>
2022-03-22 09:17:20 +0100

    feat: connect to db

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,27 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
-const { errorHandler } = require('./middlewares/errorMiddleware');
+const errorHandler = require('./middlewares/errorMiddleware');
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
+
+// Connect to Database
+connectDB()
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended : false }));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.status(200)
-    .json({ message: 'Welcome to the Support Desk API' });
+        .json({ message: 'Welcome to the Support Desk API' });
 });
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
