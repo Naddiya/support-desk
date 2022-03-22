9ba946a67bd43fb30aca36c06b07d34f858c1f99
Naddiya <nadiamedkouriz@gmail.com>
2022-03-22 14:14:28 +0100

    feat: Header & init Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Header from './components/Header';
+
+const App = () => {
   return (
-    <div className="App">
-     <h1>App</h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
