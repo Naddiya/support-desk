522d420a6265c4cf1182a03857e624ca1f5e0f14
Naddiya <nadiamedkouriz@gmail.com>
2022-03-23 12:45:03 +0100

    feat: Fetch tickets

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -24,6 +24,24 @@ export const createTicket = createAsyncThunk('tickets/create',
         }
     });
 
+// Get use tickets
+export const getTickets = createAsyncThunk('tickets/getAll',
+    async (ticketData, thunkAPI) => {
+        try {
+            const token = thunkAPI.getState().auth.user.token;
+            return await ticketService.getTickets(token);
+        } catch (error) {
+            const message =
+                (error.response
+                    && error.response.data
+                    && error.response.data.message)
+                || error.message
+                || error.toString();
+
+            return thunkAPI.rejectWithValue(message);
+        }
+    });
+
 export const ticketSlice = createSlice({
     name: 'ticket',
     initialState,
@@ -43,7 +61,20 @@ export const ticketSlice = createSlice({
                 state.isLoading = false;
                 state.isError = true;
                 state.message = action.payload;
-            });
+            })
+            .addCase(getTickets.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(getTickets.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.tickets = action.payload
+            })
+            .addCase(getTickets.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+            })
     }
 });
 
