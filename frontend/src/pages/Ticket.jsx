fe1a88073b3f0c3df0580d80a433bac64855e894
Naddiya <nadiamedkouriz@gmail.com>
2022-03-23 16:37:25 +0100

    feat: close ticket functionality

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
-import { getTicket, reset } from '../features/tickets/ticketSlice';
+import { getTicket, reset, closeTicket} from '../features/tickets/ticketSlice';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 
@@ -14,6 +14,7 @@ const Ticket = () => {
   const params = useParams();
   const dispatch = useDispatch();
   const { ticketId } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (isError) {
@@ -24,6 +25,15 @@ const Ticket = () => {
     // eslint-disable-next-line
   }, [isError, ticketId, message]);
 
+  // Close Ticket
+  const onTicketClose = () => {
+    dispatch(closeTicket(ticketId))
+    toast.success('Ticket Closed')
+
+    navigate('/tickets')
+
+  }
+
   if (isLoading) {
     return <Spinner />;
   }
@@ -31,6 +41,8 @@ const Ticket = () => {
   if (isError) {
     return <h3>Something Went Wrong</h3>;
   }
+
+
   return (
     <div className='ticket-page'>
       <header className='ticket-header'>
@@ -49,6 +61,14 @@ const Ticket = () => {
           <p>{ticket.description}</p>
         </div>
       </header>
+
+      {ticket.status !== 'closed' && (
+        <button
+          className='btn btn-block btn-danger'
+          onClick={onTicketClose}
+        >Close Ticket
+        </button>
+      )}
     </div>
   );
 };
